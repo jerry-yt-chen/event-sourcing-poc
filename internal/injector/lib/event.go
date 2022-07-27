b0c232ad0cb694c3b497517ef7563f7ff55f8864
Sean Hsu <seanhsu@17.media>
2022-07-27 11:41:05 +0800

    chore: fix after review mongoSvc -> fluentdSvc

--- internal/injector/lib/event.go
+++ internal/injector/lib/event.go
@@ -6,6 +6,6 @@ import (
 	"github.com/jerry-yt-chen/event-sourcing-poc/pkg/fluentd"
 )
 
-func InitEventPublisher(mongoSvc fluentd.Service) pubsub.Publisher {
-	return pubsub.NewPublisherDecorator(configs.C.Pub.ProjectID, configs.C.Pub.Topic, mongoSvc)
+func InitEventPublisher(fluentdSvc fluentd.Service) pubsub.Publisher {
+	return pubsub.NewPublisherDecorator(configs.C.Pub.ProjectID, configs.C.Pub.Topic, fluentdSvc)
 }
